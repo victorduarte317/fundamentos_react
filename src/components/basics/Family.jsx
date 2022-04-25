37524d6b4424fe5e00c23d8a1fd8592fa96efe84
Victor Duarte <victorctii217@gmail.com>
2022-04-25 18:59:23 -0300

    Attributing property values using different methods.

--- src/components/basics/Family.jsx
+++ src/components/basics/Family.jsx
@@ -6,7 +6,7 @@ export default props => {
         <div>
             <FamilyMember name="Peter " surname={props.surname}/> {/* This will take the surname property of "props" and apply*/}
             <FamilyMember name="Anna " {...props}/> {/* Spread takes all of "props" properties and apply'em*/}
-            <FamilyMember name="Gustav " surname="Smith"/> {/* This is a static surname attribution*/}
+            <FamilyMember name="Gustav " surname="Smith"/> {/* This is a static surname attribution */}
         </div>
     )
 }
