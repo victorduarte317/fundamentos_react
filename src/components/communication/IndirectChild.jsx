673e057a4983c61cb17064f86c544325dd089bbc
Victor Duarte <victorctii217@gmail.com>
2022-04-26 18:45:54 -0300

    Stated component

--- src/components/communication/IndirectChild.jsx
+++ src/components/communication/IndirectChild.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 export default props => {
     const cb = props.onTheClick // callback
+    const genRandomAge = () => parseInt(Math.random() * (20)) + 50
+    const genRandomMarriage = () => Math.random() > 0.5
     return (
         <div>
             <div>Child</div>
-            <button onClick={_ => cb('James', 56, true)}>
+            <button onClick={_ => cb('James', genRandomAge(), genRandomMarriage())}>
                 Send infos to Parent
             </button>
         </div>
